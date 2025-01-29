c2f80b61eabde3c7c42001d0a339d3f6d51aee34
Marco Nani <147685388+MarcoNani@users.noreply.github.com>
2025-01-29 17:00:19 +0100

    Added anti-spoofing message editing

--- public/script.js
+++ public/script.js
@@ -1,11 +1,37 @@
 const socket = io();
 const input = document.getElementById("message");
 const chat = document.getElementById("chat");
+const usernameInput = document.getElementById("username");
+const setUsernameBtn = document.getElementById("setUsername");
 
-input.addEventListener("input", () => {
-    socket.emit("typing", input.value);
+let username = "";
+let currentMessageID = undefined;
+
+let messageDictionary = {};
+
+
+
+socket.on("setUsername", (name) => { // Riceve il proprio username
+    username = name; // Imposta la variabile con il proprio username
+    usernameInput.value = name; // Imposta il campo di input con il proprio username
 });
 
-socket.on("display", (message) => {
-    chat.textContent = message;
+setUsernameBtn.addEventListener("click", () => { // Quando si clicca sul bottone per cambiare nome
+    const newName = usernameInput.value.trim(); // Prende il nuovo nome
+    if (newName) { // Se il nome non è vuoto
+        socket.emit("setUsername", newName); // Invia il nuovo nome al server
+        username = newName; // Imposta la variabile con il nuovo nome
+    }
+});
+
+input.addEventListener("input", () => { // Quando si scrive nel campo di input
+    const message_obj = { text: input.value, msg_id: currentMessageID }; // crea oggetto messaggio
+    socket.emit("typing", message_obj); // Invia il messaggio di scrittura al server
+    console.log ( "Sending message with local message id: " + message_obj.msg_id);
+});
+
+socket.on("display", (message_obj) => { // Riceve il messaggio da visualizzare
+    console.log(message_obj);
+    chat.innerHTML = `<p><strong>${message_obj.name}:</strong> ${message_obj.payload}</p>`; // Visualizza il messaggio
+    messageDictionary[message_obj.msg_id] = message_obj; // Aggiunge il messaggio al dizionario
 });
