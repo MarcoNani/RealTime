7dc7526e2721f7058d5208d1032b4698ed507168
Marco Nani <147685388+MarcoNani@users.noreply.github.com>
2025-02-22 21:32:48 +0100

    #3 - Refactored to move the sendMessage function

--- App/Android/TestUI/xml/prova/app/src/main/java/com/example/prova/MainActivity.kt
+++ App/Android/TestUI/xml/prova/app/src/main/java/com/example/prova/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -14,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    // lateinit let you declare variable without initializing them immediately
     lateinit var btnSend : Button
     lateinit var messageContent : EditText
     lateinit var messageContainer : LinearLayout
@@ -21,33 +23,40 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main) // link the UI defined in the XML to the kotlin code of the activity
+
+        // link the variable to the element by it's ID
         btnSend = findViewById<Button>(R.id.btn_send)
         messageContent = findViewById<EditText>(R.id.msg_contnt)
         messageContainer = findViewById<LinearLayout>(R.id.message_container)
 
-        btnSend.setOnClickListener(this)
+        btnSend.setOnClickListener { sendMessage(messageContent.text.toString()) } // listener at the click on the button, it exec the onClick function
+    }
+
+
+    fun sendMessage(msg: String) {
+        if (msg.isEmpty()) return
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+
+        // add a new message bubble
+        val messageBubble = TextView(this).apply{
+            text = msg
+            layoutParams = ViewGroup.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+            )
+            setPadding(16,8,16,8)
+            setBackgroundResource(android.R.drawable.dialog_holo_light_frame)
+        }
+
+        messageContainer.addView(messageBubble)
+        messageContent.text.clear()
+
+
     }
 
     override fun onClick(v: View?) {
-        var a = messageContent.text.toString()
-        when(v?.id){
-            R.id.btn_send->{
-                if (a.isNotEmpty()){
-                    val messageBubble = TextView(this).apply{
-                        text = a
-                        layoutParams = ViewGroup.LayoutParams(
-                            ViewGroup.LayoutParams.MATCH_PARENT,
-                            ViewGroup.LayoutParams.WRAP_CONTENT
-                        )
-                        setPadding(16,8,16,8)
-                        setBackgroundResource(android.R.drawable.dialog_holo_light_frame)
-                    }
 
-                    messageContainer.addView(messageBubble)
-                    messageContent.text.clear()
-                }
-            }
-        }
     }
 }
