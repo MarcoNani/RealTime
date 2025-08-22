35896e6f442769190aeaed2fef3fffbb9c2663f8
Marco Nani <147685388+MarcoNani@users.noreply.github.com>
2025-08-22 19:56:36 +0200

    Fixed the main index that was no more present

--- App/web/assets/js/buttadentro.js
+++ App/web/assets/js/buttadentro.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const serverUrl = localStorage.getItem("serverUrl");
     const apiKey = localStorage.getItem("apiKey");
 
-    if (!serverUrl || !apiKey) {
-        window.location.href = "../onboarding/";
+    if (serverUrl && apiKey) {
+        window.location.href = "./chat_list/";
+    } else {
+        window.location.href = "./onboarding/";
     }
 });
