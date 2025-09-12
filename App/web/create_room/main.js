f6b4226f985ee48d0cbdd198c4434cae403d0a9f
Marco Nani <147685388+MarcoNani@users.noreply.github.com>
2025-09-12 16:24:12 +0200

    Improved graphics and pages names

--- App/web/create_room/main.js
+++ App/web/create_room/main.js
@@ -4,7 +4,7 @@ function go_to_part2() {
     // hide the part 1 section
     document.getElementById('generate').style.display = 'none';
     // change the h2 title
-    document.getElementById('page-title').innerText = 'New Room part 2';
+    document.getElementById('page-title').innerText = 'Create room part 2/2';
     // show the part 2 section
     document.getElementById('accept').style.display = 'block';
 }
